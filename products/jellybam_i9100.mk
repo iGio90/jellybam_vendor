aa4760991c06836a7241943a35c20be926c58545
igio90 <giovanni.rocca.90@gmail.com>
2013-02-13 16:50:29 +0100

    Derp

--- products/jellybam_i9100.mk
+++ products/jellybam_i9100.mk
@@ -7,10 +7,6 @@ OVERLAY_TARGET := pa_hdpi
 # AOKP device overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/jellybam/overlay/aokp/device/i9100
 
-# JELLYBAM bootanimation
-PRODUCT_COPY_FILES += \
-    vendor/jellybam/prebuilt/common/bootanimation/hdpi.zip:system/media/bootanimation.zip
-
 # include JELLYBAM common configuration
 include vendor/jellybam/config/jellybam_common.mk
 
@@ -18,6 +14,10 @@ include vendor/jellybam/config/jellybam_common.mk
 $(call inherit-product, device/samsung/i9100/cm.mk)
 
 PRODUCT_NAME := jellybam_i9100
+
+# JELLYBAM bootanimation
+PRODUCT_COPY_FILES += \
+    vendor/jellybam/prebuilt/common/bootanimation/hdpi.zip:system/media/bootanimation.zip
 
 # Update local_manifest.xml
 GET_PROJECT_RMS := $(shell vendor/jellybam/tools/removeprojects.py $(PRODUCT_NAME))
