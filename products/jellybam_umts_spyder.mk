bf12ee68924cf13b5620e5c1a26ee422f569badd
igio90 <giovanni.rocca.90@gmail.com>
2013-04-16 02:13:08 +0200

    Spyder and umts spyder (1/2) Signed-off-by: igio90 <giovanni.rocca.90@gmail.com>

---
+++ products/jellybam_umts_spyder.mk
@@ -0,0 +1,23 @@
+# Check for target product
+ifeq (jellybam_umts_spyder,$(TARGET_PRODUCT))
+
+# OVERLAY_TARGET adds overlay asset source
+OVERLAY_TARGET := pa_hdpi
+
+# AOKP device overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/jellybam/overlay/aokp/device/common
+
+# include JELLYBAM common configuration
+include vendor/jellybam/config/jellybam_common.mk
+
+# Inherit CM device configuration
+$(call inherit-product, device/motorola/umts_spyder/cm.mk)
+
+PRODUCT_NAME := jellybam_umts_spyder
+
+# Update local_manifest.xml
+GET_PROJECT_RMS := $(shell vendor/jellybam/tools/removeprojects.py $(PRODUCT_NAME))
+GET_PROJECT_ADDS := $(shell vendor/jellybam/tools/addprojects.py $(PRODUCT_NAME))
+
+endif
+
