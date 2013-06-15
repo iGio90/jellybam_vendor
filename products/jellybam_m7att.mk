10ed32c0b3eae4d0ad38875260b1dfc9573926fc
igio90 <giovanni.rocca.90@gmail.com>
2013-06-15 15:03:14 +0200

    Build all HTC One variants Signed-off-by: igio90 <giovanni.rocca.90@gmail.com>

---
+++ products/jellybam_m7att.mk
@@ -0,0 +1,23 @@
+# Check for target product
+ifeq (jellybam_m7att,$(TARGET_PRODUCT))
+
+# OVERLAY_TARGET adds overlay asset source
+OVERLAY_TARGET := pa_xxhdpi
+
+# AOKP device overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/jellybam/overlay/aokp/device/common
+
+# include JELLYBAM common configuration
+include vendor/jellybam/config/jellybam_common.mk
+
+# Inherit CM device configuration
+$(call inherit-product, device/htc/m7att/cm.mk)
+
+PRODUCT_NAME := jellybam_m7att
+
+# Update local_manifest.xml
+GET_PROJECT_RMS := $(shell vendor/jellybam/tools/removeprojects.py $(PRODUCT_NAME))
+GET_PROJECT_ADDS := $(shell vendor/jellybam/tools/addprojects.py $(PRODUCT_NAME))
+
+endif
+
